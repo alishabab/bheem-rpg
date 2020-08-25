a9073681fae35c8397f51345095d7668b07fed92
Shabab <shababsaifi@gmail.com>
2020-08-25 13:57:14 +0530

    Added audio assets and loaded them on boot scene

--- src/Scenes/BootScene.js
+++ src/Scenes/BootScene.js
@@ -6,7 +6,35 @@ export default class BootScene extends Phaser.Scene {
   }
 
   preload () {
+     this.loadImages();
+     this.loadSpriteSheets();
+     this.loadAudio();
+     this.loadTileMap();
+  }
+
+  loadImages() {
     this.load.image('logo', 'assets/images/zenva_logo.png');
+    this.load.image('button1', 'assets/images/ui/blue_button01.png');
+    this.load.image('button2', 'assets/images/ui/blue_button02.png');
+    this.load.image('background', 'assets/level/background-extruded.png');
+  }
+
+  loadSpriteSheets() {
+    this.load.spritesheet('items', 'assets/images/items.png', { frameWidth: 32, frameHeight: 32 });
+    this.load.spritesheet('characters', 'assets/images/characters.png', { frameWidth: 32, frameHeight: 32 });
+    this.load.spritesheet('monsters', 'assets/images/monsters.png', { frameWidth: 32, frameHeight: 32 });
+  }
+
+  loadAudio() {
+    this.load.audio('goldSound', ['assets/audio/Pickup.wav']);
+    this.load.audio('enemyDeath', ['assets/audio/EnemyDeath.wav']);
+    this.load.audio('playerAttack', ['assets/audio/PlayerAttack.wav']);
+    this.load.audio('playerDamage', ['assets/audio/PlayerDamage.wav']);
+    this.load.audio('playerDeath', ['assets/audio/PlayerDeath.wav']);
+  }
+
+  loadTileMap() {
+    this.load.tilemapTiledJSON('map', 'assets/level/large_level2.json');
   }
 
   create () {
