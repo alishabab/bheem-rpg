41788275f2825c8b0c10dabf31ed4a85ea673e0d
Shabab <shababsaifi@gmail.com>
2020-08-25 16:34:28 +0530

    Added Player class

--- src/Scenes/GameScene.js
+++ src/Scenes/GameScene.js
@@ -13,10 +13,17 @@ export default class GameScene extends Phaser.Scene {
   create () {
     this.createMap();
   }
-
-createMap() {
-  this.map = this.make.tilemap({ key: 'map' });
-  this.tiles = this.map.addTilesetImage('background', 'background', 32, 32, 1, 2);
-  this.backgroundLayer = this.map.createStaticLayer('background', this.tiles, 0, 0);
+  
+  createMap() {
+    // create the tile map
+    this.map = this.make.tilemap({ key: 'map' });
+    // add the tileset image to our map
+    this.tiles = this.map.addTilesetImage('background', 'background', 32, 32, 1, 2);
+    // create our background
+    this.backgroundLayer = this.map.createStaticLayer('background', this.tiles, 0, 0);
+    this.backgroundLayer.setScale(2);    // New code
+    // create blocked layer
+    this.blockedLayer = this.map.createStaticLayer('blocked', this.tiles, 0, 0);
+    this.blockedLayer.setScale(2);    // New code
   }
 };
