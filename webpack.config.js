268fc7ab1701715587f02f0402be1bbabf4c3a52
Shabab <shababsaifi@gmail.com>
2020-08-25 13:07:21 +0530

    Added phaser template with boot, credit and options scene

--- webpack.config.js
+++ webpack.config.js
@@ -5,11 +5,10 @@ const path = require('path');
 
 module.exports = {
 
-    entry: './src/main.js',
+    entry: './src/index.js',
 
     output: {
-        path: path.resolve(__dirname, 'build'),
-        publicPath: '/dist/',
+        path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
     },
 
