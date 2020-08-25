ddded68643a1d3c92eda21c04e42575778d64236
Shabab <shababsaifi@gmail.com>
2020-08-25 13:14:30 +0530

    Added level assets from tiled

--- src/Config/config.js
+++ src/Config/config.js
@@ -1,8 +1,22 @@
-import 'phaser';
+import Phaser from 'phaser';
 
 export default {
   type: Phaser.AUTO,
   parent: 'phaser-example',
   width: 800,
-  height: 600
+  height: 600,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      debug: false,
+      gravity: {
+        y: 0,
+      },
+    },
+  },
+  pixelArt: true,
+  roundPixels: true,
+  dom: {
+    createContainer: true,
+  },
 };
